e291b0c4c515edbb1833533d8314278dc36638c1
saurabh <dekapurwars@gmail.com>
2021-08-30 16:59:33 +0530

    calculating monthly wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -8,15 +8,16 @@ public class EmployeeWageComputation {
 			 int empWagePerHour=20;
 				int fullDayHour=8;
 				int halfDayHour=4;
+				int workingDaysPerMonth=20;
 			 int empcheck =(int) Math.floor(Math.random()*10)%3;
 			switch(empcheck) {
-			case 1: int dailyWage=empWagePerHour*fullDayHour;
-				    System.out.println("Daily wage of employee in full time is "+dailyWage);
+			case 1: int dailyWage=empWagePerHour*fullDayHour*workingDaysPerMonth;
+				    System.out.println(" Monthly wage of employee in full time is "+dailyWage);
 			break;
-			case 2: int PartTimeWage=empWagePerHour*halfDayHour;
-				System.out.println("Daily wage of employee in part time is "+PartTimeWage);
-			
-			default:System.out.println("As employee is absent so his daily wage is 0");
+			case 2: int PartTimeWage=empWagePerHour*halfDayHour*workingDaysPerMonth;
+				System.out.println("Monthly wage of employee in part time is "+PartTimeWage);
+			break;
+			default:System.out.println("As employee is absent so his monthly wage is 0");
 			}
 		}
 }
