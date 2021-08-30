79a8847a04aaddaf4c4160718e4bffe5775459a8
saurabh <dekapurwars@gmail.com>
2021-08-30 17:03:13 +0530

    added monthly salary in till condition

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,23 +1,22 @@
 
 public class EmployeeWageComputation {
 	public static void main(String[] arg) {
-		checkEmpPresentorAbsentWage();
+
+		MonthlySalary();
+
 	}
-		
-		static void checkEmpPresentorAbsentWage() {
-			 int empWagePerHour=20;
-				int fullDayHour=8;
-				int halfDayHour=4;
-				int workingDaysPerMonth=20;
-			 int empcheck =(int) Math.floor(Math.random()*10)%3;
-			switch(empcheck) {
-			case 1: int dailyWage=empWagePerHour*fullDayHour*workingDaysPerMonth;
-				    System.out.println(" Monthly wage of employee in full time is "+dailyWage);
-			break;
-			case 2: int PartTimeWage=empWagePerHour*halfDayHour*workingDaysPerMonth;
-				System.out.println("Monthly wage of employee in part time is "+PartTimeWage);
-			break;
-			default:System.out.println("As employee is absent so his monthly wage is 0");
-			}
+
+	static void MonthlySalary() {
+		int empWagePerHour = 20;
+		int empHrs = 8;
+		int WorkingDays = 1;
+		int MonthlySalary = 0;
+		while (WorkingDays < 21) {
+			MonthlySalary = empWagePerHour * empHrs * WorkingDays;
+			MonthlySalary += MonthlySalary;
+			WorkingDays++;
 		}
+		System.out.println("Monthly salary of employee if present: " + MonthlySalary);
+	}
+
 }
